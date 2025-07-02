b8396bb419625c97d91cbe3f28ec6d264f538beb
Saurabh Joshi <saurabhjoshi7869@gmail.com>
2025-07-02 10:09:09 +0530

    renamed spark app name

--- 10.unique_sorted_records.py
+++ 10.unique_sorted_records.py
@@ -20,7 +20,7 @@ if __name__ == "__main__":
     spark = (
         SparkSession.
         builder.
-        appName('JobStageTaskExample').
+        appName('tutorial').
         getOrCreate()
     )
 
